991c26846f238043291f0c4d188c546ca7ba1fd6
foresaa <andy.forrester2@btopenworld.com>
2024-10-21 22:34:58 +0100

    Commit before switching branches

--- scripts/run_optimization.py
+++ scripts/run_optimization.py
@@ -8,31 +8,30 @@ import re
 from apps.ms_project_interface import open_ms_project, update_project_with_optimized_schedule, save_project_as_optimized
 from apps.optimization import optimize_schedule
 
-def run_optimization():
-    """
-    Runs the optimization on an MS Project file.
-    - Opens the MS Project file selected by the user.
-    - Runs the optimization process.
-    - Updates the MS Project schedulclse with optimized dates.
-    - Saves a new version of the project file with the optimized schedule.
-    """
-    project, tasks, original_file_path = open_ms_project()  # User selects the file through a dialog
+def run_optimization(): 
+    # Open the MS Project file and extract task data
+    project, tasks, original_file_path = open_ms_project()
     
-    if project and tasks:
-        project_start = project.ProjectStart  # Get project start date
-        hours_per_day = project.HoursPerDay   # Fetch the hours per day for the project
-        
-        # Run the optimization process, passing the project start date and hours per day
-        optimized_schedule = optimize_schedule(tasks, project_start, hours_per_day, min_duration_percentage=0.95, max_duration_percentage=1.05)
+    # Define the project parameters
+    project_start = project.ProjectStart
+    hours_per_day = project.HoursPerDay  # Assuming project has this attribute
+    
+    # Define optimization parameters
+    min_duration_percentage = 0.95  # Example: 95% of the original duration
+    max_duration_percentage = 1.05  # Example: 105% of the original duration
+    avoid_overallocation    = True  # Set this to True to avoid overallocation during optimization
+    
+    # Run the optimization process
+    optimized_schedule = optimize_schedule(tasks, project_start, hours_per_day, 
+                                           min_duration_percentage, max_duration_percentage, 
+                                           avoid_overallocation)
 
-        if optimized_schedule:
-            update_project_with_optimized_schedule(project, optimized_schedule,hours_per_day)
-            save_project_as_optimized(project, original_file_path)
-            print(f"Optimization complete and project saved as {original_file_path}_opt.mpp.")
-        else:
-            print("Optimization was not applied.")
-    else:
-            print("No project file or tasks found.")
+    # Update the project with the optimized schedule
+    update_project_with_optimized_schedule(project, optimized_schedule, hours_per_day)
+
+    # Save the optimized project as a new file
+    save_project_as_optimized(project, original_file_path)
+
 
 if __name__ == "__main__":
     run_optimization()
